3a950b6b93faed5a55797ff38d50dd24d9c3b523
Bassaved26 <andreshernandezsaa0726@gmail.com>
2025-11-11 22:00:40 -0400

    random-card-finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.body.onload = function (){
+    
+    let figuras = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    let emojis = ['♦','♥','♠','♣'];
+
+    let numero = Math.floor((Math.random() * figuras.length));
+    let simboloNumero = Math.floor((Math.random() * emojis.length));
+    
+    let color = ""
+    for(let i=0; i<emojis.length; i++){
+      if(emojis[simboloNumero] === '♦') color = "diamond"
+      else if(emojis[simboloNumero] === '♥') color="heart"
+      else if(emojis[simboloNumero] === '♠') color="spade"
+      else if(emojis[simboloNumero] === '♣') color="club"  
+    }
+
+    document.querySelector('#plantillaHTML').innerHTML = `<div class="col-sm-12 card ${color} position-relative">  
+            <div class=" top-left d-block"><div class="figuras">${emojis[simboloNumero]}</div></div>
+            <div class="middle-left d-block"><h1>${figuras[numero]}</h1></div>  
+            <div class="bottom-right d-block"><div class="figuras">${emojis[simboloNumero]}</div></div>
+          </div>`
+
+    document.querySelector(".middle-left").style.color ="#000";
+  }
+
+
+
